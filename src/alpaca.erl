a6662bc3d8bc38b482570bd190a2487f4d431a33
Jeremy Pierre <j.14159@gmail.com>
2016-12-11 20:43:11 -0800

    Cleaning up alpaca-formatter-in-alpaca
    It's still not doing anything helpful but I've started breaking things
    down a bit and a number of the fixes over the last couple of days have
    smoothed things out enough to show where I'm going.  Contents of the
    folder `src/lib` are helper functions, not necessarily a good start on
    anything approaching a standard library.

--- src/alpaca.erl
+++ src/alpaca.erl
@@ -350,13 +350,4 @@ allow_duplicate_definition_with_different_arity_test() ->
     ?assertEqual([0, 1, 2, 3], M:seq(3)),
     code:delete(M).
 
-%% There seems to be a compilation bug in the early formatter work I'm trying
-%% using Alpaca to write its own code formatter.  Figured I might as well just
-%% add the test here.
-own_formatter_test() ->
-    Files = ["src/alpaca_native_ast.alp", "src/alpaca_format.alp"],
-    [M1, M2] = compile_and_load(Files, []),
-    code:delete(M1),
-    code:delete(M2).
-
 -endif.
---
+++ src/lib/README.md
@@ -0,0 +1,1 @@
+Mostly some simple supporting modules and functions as we have no standard library yet.  This is likely going to be a bit chaotic until we settle on particular conventions and style.
